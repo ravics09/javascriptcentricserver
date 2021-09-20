face30e4745b3dc6278277bda32b0a162d3cb3d7
RAVI SHARMA <ravics09@RAVIs-MacBook-Air.local>
2021-09-20 17:11:59 +0530

    Added singin user service with jwt token

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -2,17 +2,17 @@ const express = require('express');
 const mainRoutes = express.Router();
 const userServices = require('./../services/userServices');
 
-mainRoutes.post('/signup', signup);
-mainRoutes.post('/signin', signin);
+mainRoutes.post('/signup', signUp);
+mainRoutes.post('/signin', signIn);
 mainRoutes.use('/', (request, response, next)=> {
     response.status(404).json({error:"Page Not Found"});
 });
 
-function signup(request, response, next) {
+function signUp(request, response, next) {
     userServices.createUser(request.body, response, next);
 };
 
-function signin(request, response, next) {
+function signIn(request, response, next) {
     userServices.getUser(request.body, response, next);
 };
 
--- services/userServices.js
+++ services/userServices.js
@@ -1,15 +1,21 @@
 const db = require("./../database/createConnection");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const User = db.User;
 
-module.exports = { createUser };
+module.exports = { createUser, getUser };
 
 async function createUser(userDetails, response, next) {
+  //Find for email if user already registered or not?
   User.findOne({
     email: userDetails.email,
   }).then((dbUser) => {
     if (dbUser) {
-      return response.status(409).json({ message: "User with this email address already registered, Please signIn..." });
+      return response.status(409).json({
+        message:
+          "User with this email address already registered, Please signIn...",
+      });
+      //If user not registered then create new user entry in database.
     } else if (userDetails.email && userDetails.password) {
       bcrypt.hash(userDetails.password, 12, (err, passwordHash) => {
         if (err) {
@@ -23,7 +29,9 @@ async function createUser(userDetails, response, next) {
             hash: passwordHash,
           })
             .then(() => {
-              response.status(200).json({ message: "You have signUp successfully. Please signIn here !!" });
+              response.status(200).json({
+                message: "You have signUp successfully. Please signIn here !!",
+              });
             })
             .catch((err) => {
               console.log(err);
@@ -37,7 +45,47 @@ async function createUser(userDetails, response, next) {
     } else if (!userDetails.password) {
       return response.status(400).json({ message: "Please Enter Password" });
     } else if (!userDetails.email) {
-      return response.status(400).json({ message: "Please Enter Email Address" });
+      return response
+        .status(400)
+        .json({ message: "Please Enter Email Address" });
+    }
+  });
+}
+
+async function getUser(userDetails, response, next) {
+  // Find for email if user registered or not?
+  User.findOne({ email: userDetails.email }).then((dbUser) => {
+    if (!dbUser) {
+      return response.status(404).json({ message: "User Not Registered." });
+    } else {
+      // If User Registered then retrive user details and compare password.
+      // password hash
+      console.log("dbUser=====", dbUser);
+      bcrypt.compare(
+        userDetails.password,
+        dbUser.hash,
+        (err, compareRes) => {
+          if (err) {
+            // error while comparing
+            response
+              .status(502)
+              .json({ message: "Server error while checking user password" });
+          } else if (compareRes) {
+            // password match
+            const token = jwt.sign({ email: userDetails.email }, "secret", {
+              expiresIn: "1h",
+            });
+            response
+              .status(200)
+              .json({ message: "User successfully signin", token: token });
+          } else {
+            // password doesnt match
+            response
+              .status(401)
+              .json({ message: "Invalid Credentials! Please try again." });
+          }
+        }
+      );
     }
   });
 }
